32b47d47c3ff679c0a4a12b348fa63d0ec252ac9
Thev2Andy <ciprian.andrei002@gmail.com>
2024-04-15 18:58:05 +0300

    Removed old / unused namespaces.

--- Library/PowerLog/Source/Core/Formatter/Formatter/Format.cs
+++ Library/PowerLog/Source/Core/Formatter/Formatter/Format.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 
--- Library/PowerLog/Source/Core/Logger/Data/Arguments/Arguments.cs
+++ Library/PowerLog/Source/Core/Logger/Data/Arguments/Arguments.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
 namespace PowerLog
 {
